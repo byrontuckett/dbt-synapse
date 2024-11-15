763a79f08ad6c5fce4f6a66f492b3e4fedf79d49
Byron Tuckett <byrontuckett@outlook.com>
2024-11-15 11:13:09 +1000

    feat: update test helpers.sql to use tmp tables

--- dbt/include/synapse/macros/materializations/tests/helpers.sql
+++ dbt/include/synapse/macros/materializations/tests/helpers.sql
@@ -1,19 +1,30 @@
 {% macro synapse__get_test_sql(main_sql, fail_calc, warn_if, error_if, limit) -%}
   {% set target_schema = var('synapse_test_schema', generate_schema_name() ) %}
 
-  -- Create target schema in synapse db if it does not
-  IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = '{{ target_schema }}')
-  BEGIN
-    EXEC('CREATE SCHEMA [{{ target_schema }}]')
-  END
+  {# Check if the main_sql contains a WITH statement #}
+  {% set with_statement = 'with\s+(\w+)\s+as' -%}
+  {% set re = modules.re %}
+  {% set is_match = re.match(with_statement, main_sql, re.IGNORECASE) -%}
 
-  {% if main_sql.strip().lower().startswith('with') %}
-    {% set testview %}
-      {{ target_schema }}.testview_{{ range(1300, 19000) | random }}
-    {% endset %}
+  {% if is_match -%}
 
-    {% set sql = main_sql.replace("'", "''")%}
-    EXEC('create view {{testview}} as {{ sql }};')
+    {% set temp_table_name -%}
+      #dbttesttmp_{{ range(1300, 19000) | random }}
+    {% endset -%}
+
+    {# Escape single quotes #}
+    {% set sql = main_sql.replace("'", "''") -%}
+
+    {# Check temp table existence before dropping #}
+    EXEC('if object_id(n''tempdb..{{ temp_table_name }}'') is not null drop table {{ temp_table_name }};')
+
+    EXEC('create table {{ temp_table_name }}
+    with
+    ( distribution = round_robin
+      , heap )
+    as
+    {{ sql }};')
+
     select
       {{ "top (" ~ limit ~ ')' if limit != none }}
       {{ fail_calc }} as failures,
@@ -22,12 +33,14 @@
       case when {{ fail_calc }} {{ error_if }}
         then 'true' else 'false' end as should_error
     from (
-      select * from {{testview}}
+      select * from {{ temp_table_name }}
     ) dbt_internal_test;
 
-    EXEC('drop view {{testview}};')
+    EXEC('drop table {{ temp_table_name }};')
 
   {% else -%}
+
+    {# No CTE so proceed with statement as a subquery #}
     select
       {{ "top (" ~ limit ~ ')' if limit != none }}
       {{ fail_calc }} as failures,
